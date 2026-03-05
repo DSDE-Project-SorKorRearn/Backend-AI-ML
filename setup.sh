57cc826507f6ce366d6215671bee1809f45daadc
Premkub49 <128142958+Premkub49@users.noreply.github.com>
2026-03-05 14:19:13 +0700

    Add CSV loading script to setup process
    Added command to load CSV data into SQLite after setup.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ python -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
 wget -P ./data/ https://blob.ngixx.me/processed_traffy_data_for_visualization.csv ./data/
-
+python scripts/load_csv_to_sqlite.py
 echo "
     Next Steps
     1) run all ai/train_model.ipynb
